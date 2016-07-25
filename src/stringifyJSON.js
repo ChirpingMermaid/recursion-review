f23ed4f7ce2e4ba8720999852ec4cc8dccdb08a2
chirpingmermaid <bluejello@gmail.com>
2016-07-25 16:42:27 -0700

    Review and complete specs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,49 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+/*START SOLUTION*/
+
+
+/*END SOLUTION*/
 var stringifyJSON = function(obj) {
   // your code goes here
+  /*START SOLUTION*/
+  var type = typeof obj;
+  var string = '';
+  if (type === 'function' || type === 'undefined') {
+    return '';
+  }
+  if (type === 'string') {
+    return '"' + obj + '"';
+  }
+  if (type === 'number' || type === 'boolean') {
+    return String(obj);
+  }
+  if (type === 'object') {
+    if (obj === null) {
+      return 'null';
+    }
+    if (Array.isArray(obj)) {
+      string += '[';
+      string += obj.map(function(element) {
+        return stringifyJSON(element);
+      });
+      string += ']';
+    } else {
+      string += '{';
+      for (var key in obj) {
+        if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+          var keys = Object.keys(obj);
+          if (key === keys[keys.length - 1]) {
+            string += '"' + String(key) + '":' + stringifyJSON(obj[key]);
+          } else {
+            string += '"' + String(key) + '":' + stringifyJSON(obj[key]) + ',';
+          }
+        }
+      }
+      string += '}';
+    }
+    return string;
+  }
+  /*END SOLUTION*/
 };
